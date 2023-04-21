a44b7df66a80703879cb3959a9062c167864f609
alexander <sashakb@mail.ru>
2023-04-21 20:04:43 +0300

    initial commit

---
+++ src/main/java/com/dulepov/spring/rest/boot_rest/BootRestApplication.java
@@ -0,0 +1,13 @@
+package com.dulepov.spring.rest.boot_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestApplication.class, args);
+	}
+
+}
